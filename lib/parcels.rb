d7b3c1d06a52409e06647603b2403d37944b1748
Marina Neumann & Peggy Condon <student@epicodus.com>
2014-07-30 10:46:20 -0700

    cost to ship passing

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -10,11 +10,26 @@ class Parcel
    def volume
     # length*width*height
     @length * @width* @height
-
    end
+
+  def cost_to_ship
+    if 0 < @weight && @weight < 1.0
+        price_per_pound = 5.00
+    elsif 1.0 < @weight && @weight < 3.0
+       price_per_pound=3.00
+    elsif 3.0 < @weight && @weight < 10.0
+       price_per_pound = 2.00
+    else
+      puts "FREE SHIPPING"
+      end
+
+    @weight * price_per_pound
+  end
 end
 
-#  parcel = Parcel.new(12,23,2,2)
+# parcel = Parcel.new(12,23,2,2)
 
 # a = parcel.volume(12,23,2)
 # puts "Volume is : #{a}"
+# b = parcel.cost_to_ship(0.75)
+# puts "Cost to ship is : #{b}"
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,13 +3,18 @@ require 'parcels'
 
 describe Parcel do
   it 'is initialized with parcel measurements' do
-    test_parcel = Parcel.new(12, 23, 2, 22)
+    test_parcel = Parcel.new(12, 23, 2, 0.75)
     test_parcel.should be_an_instance_of Parcel
   end
 
   it 'calculates the volume for the parcel' do
-    test_parcel = Parcel.new(12,23,2,22)
+    test_parcel = Parcel.new(12,23,2,0.75)
     test_parcel.volume.should eq 552
+  end
+
+  it 'calculates cost_to_ship based on formula'do
+    test_parcel = Parcel.new(12,23,2,0.75)
+    test_parcel.cost_to_ship.should eq 3.75
   end
 end
 
@@ -22,5 +27,10 @@ end
 # 4)When you call the cost_to_ship method on your parcel,
 #   return a cost based on a formula you make up.
 # 5)Build a program that lets users calculate shipping costs.
+
+# formula
+# if 0<parcel.weight<1.0    price_per_pound=5.00
+# if 1.0<pacel.weight<3.0   price_per_pound=3.00
+# if 3.0<pacel.weight<10.0   price_per_pound=2.00
 
 
